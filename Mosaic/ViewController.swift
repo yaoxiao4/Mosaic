37871ed42dca1d8259d7674dfd965c780d45b376
Ali Reza <alirezatr@hotmail.com>
2015-05-25 15:32:34 -0400

    Initial creation of project

---
+++ Mosaic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mosaic
+//
+//  Created by Ali on 2015-05-25.
+//  Copyright (c) 2015 CS446. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
