67ec6965a2d5c7808784b2fb37a9955c227c2efc
Yao <yaoxiao4@hotmail.com>
2015-06-11 01:02:27 -0400

    event details view skeleton done

--- Mosaic/EventDetailsViewController.swift
+++ Mosaic/EventDetailsViewController.swift
@@ -10,9 +10,8 @@ import Foundation
 import UIKit
 class EventDetailsViewController: UIViewController {
     
-    var eventTitle: String? = nil
-    var eventDescription: String? = nil
     var event: Event? = nil
+    var descriptionScrollView: UIScrollView!
     
     required init(coder aDecoder: NSCoder) {
         super.init(nibName: nil, bundle: nil)
@@ -26,6 +25,8 @@ class EventDetailsViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
+ 
+        
         self.view.backgroundColor = UIColor.groupTableViewBackgroundColor()
         self.title = "Event Details"
         //self.navigationItem.hidesBackButton = true
@@ -39,34 +40,101 @@ class EventDetailsViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = barButton
         
         // This block handles the title
-        let eventTitleLabel = UILabel(frame: CGRectMake(self.view.frame.width/2 - 80,80,200,80))
+        let eventTitleLabel = UILabel(frame: CGRectMake(self.view.frame.width/2 - 90, 90, 225, 10))
         eventTitleLabel.text =  self.event?.title
-        eventTitleLabel.textAlignment = .Center;
+        eventTitleLabel.textAlignment = NSTextAlignment.Left;
         eventTitleLabel.lineBreakMode = .ByWordWrapping;
         eventTitleLabel.numberOfLines = 0;
         eventTitleLabel.sizeToFit();
+        eventTitleLabel.font = UIFont(name:"HelveticaNeue-Bold", size: 16.0)
         self.view.addSubview(eventTitleLabel)
         
-        // This block handles the description
-        let eventDetailsBox = UIView(frame: CGRectMake(20, 140, self.view.frame.width-40, 100))
-        eventDetailsBox.backgroundColor = UIColor.lightGrayColor()
-        eventDetailsBox.layer.cornerRadius = 30.0
+        ///////////////
+        //   BEGIN DETAILS
+        ///////////////
+        // This block handles the details box
+        let eventDetailsBox = UIView(frame: CGRectMake(0, 120 + eventTitleLabel.frame.height, self.view.frame.width, 120))
+        eventDetailsBox.backgroundColor = UIColor.whiteColor()
         
-        let eventDetailsLabel = UILabel(frame: CGRectMake(25,15,200,80))
-        eventDetailsLabel.text = "good Morning"
-        eventDetailsLabel.textColor = UIColor.whiteColor()
-        eventDetailsLabel.textAlignment = .Center;
-        eventDetailsLabel.lineBreakMode = .ByWordWrapping;
-        eventDetailsLabel.numberOfLines = 0;
-        eventDetailsLabel.sizeToFit();
-        eventDetailsBox.addSubview(eventDetailsLabel)
+        // For Location
+        let eventLocationLabel = UILabel(frame: CGRectMake(85, 10, 200, 30))
+        eventLocationLabel.text = self.event!.location
+        eventLocationLabel.textAlignment = .Left;
+        eventDetailsBox.addSubview(eventLocationLabel)
+        let locationIconView = UIImageView(frame: CGRectMake(35, 10, 28, 23))
+        var locationIcon = UIImage(named: "location-icon.png")
+        locationIconView.image = locationIcon
+        eventDetailsBox.addSubview(locationIconView)
+        
+        // For Date
+        let eventDateLabel = UILabel(frame: CGRectMake(85, eventLocationLabel.frame.origin.y + 35,200,30))
+        let dateFormatter = NSDateFormatter()
+        var theDateFormat = NSDateFormatterStyle.ShortStyle //5
+        let theTimeFormat = NSDateFormatterStyle.ShortStyle//6
+        dateFormatter.dateStyle = theDateFormat//8
+        dateFormatter.timeStyle = theTimeFormat//9
+        eventDateLabel.text = dateFormatter.stringFromDate(self.event!.date)
+        eventDateLabel.textAlignment = .Left;
+        let dateIconView = UIImageView(frame: CGRectMake(35, eventLocationLabel.frame.origin.y + 35, 28, 28))
+        var dateIcon = UIImage(named: "calendar-icon.png")
+        dateIconView.image = dateIcon
+        eventDetailsBox.addSubview(dateIconView)
+
+        eventDetailsBox.addSubview(eventDateLabel)
+        
+        // For Time
+        let eventTimeLabel = UILabel(frame: CGRectMake(85, eventDateLabel.frame.origin.y + 35, 200, 30))
+        eventTimeLabel.text = "2:30PM - 3:30PM"
+        eventTimeLabel.textAlignment = .Left;
+        eventDetailsBox.addSubview(eventTimeLabel)
+        let timeIconView = UIImageView(frame: CGRectMake(35, eventDateLabel.frame.origin.y + 35, 28, 28))
+        var timeIcon = UIImage(named: "clock-icon.png")
+        timeIconView.image = timeIcon
+        
+        eventDetailsBox.addSubview(timeIconView)
+        
         self.view.addSubview(eventDetailsBox)
         
+        /////////// END EVENT DETAILS
+        
         // This block handles the FB icon (if it is a FB Event)
-        var imageView = UIImageView(frame: CGRectMake(self.view.frame.width/8 - 20, 75, 40, 40));
-        var image = UIImage(named: "facebook-icon.png");
-        imageView.image = image;
-        self.view.addSubview(imageView);
+        var fbIconView = UIImageView(frame: CGRectMake(self.view.frame.width/8 - 20, 70 + eventTitleLabel.frame.height/2, 40, 40));
+        var fbIcon = UIImage(named: "facebook-icon.png");
+        fbIconView.image = fbIcon;
+        self.view.addSubview(fbIconView);
+        
+        // This block handles the bookmark icon
+        var bookmarkView = UIImageView(frame: CGRectMake(eventTitleLabel.frame.width + eventTitleLabel.frame.origin.y + 50, 75 + eventTitleLabel.frame.height/2, 30, 30));
+        var bookmarkIcon = UIImage(named: "star-empty.png");
+        bookmarkView.image = bookmarkIcon;
+        self.view.addSubview(bookmarkView);
+        
+        // This block handles the title
+        
+        let eventDescriptionLabel = UILabel(frame: CGRectMake(25, 20, self.view.frame.width - 50, 30))
+        eventDescriptionLabel.text =  self.event?.details
+        eventDescriptionLabel.textAlignment = NSTextAlignment.Left;
+        eventDescriptionLabel.lineBreakMode = .ByWordWrapping;
+        eventDescriptionLabel.numberOfLines = 0;
+        eventDescriptionLabel.font = UIFont(name:"HelveticaNeue-Bold", size: 12.0)
+        eventDescriptionLabel.sizeToFit();
+        
+        // This block handles the details box
+        let eventDescriptionBox = UIView(frame: CGRectMake(0, 0, self.view.frame.width, eventDescriptionLabel.bounds.height + 40))
+        eventDescriptionBox.backgroundColor = UIColor.whiteColor()
+        
+        eventDescriptionBox.addSubview(eventDescriptionLabel)
+
+        
+        
+        descriptionScrollView = UIScrollView(frame: CGRectMake(0, 20 + eventDetailsBox.frame.height + eventDetailsBox.frame.origin.y, eventDescriptionBox.frame.width, eventDescriptionBox.frame.height))
+        descriptionScrollView.backgroundColor = UIColor.blackColor()
+        // 3
+        descriptionScrollView.contentSize = CGSize(width: self.view.frame.width, height: 500)
+        // 4
+        descriptionScrollView.addSubview(eventDescriptionBox)
+        descriptionScrollView.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(descriptionScrollView)
     }
     
     @IBAction func close(sender: AnyObject) {
