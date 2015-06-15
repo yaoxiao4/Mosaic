998aa1c4de0df4298a3369f6337778ba15ecd22a
Ali Reza <alirezatr@hotmail.com>
2015-06-15 13:01:28 -0400

    Minor changes to the settings page

--- Mosaic/SettingsViewController.swift
+++ Mosaic/SettingsViewController.swift
@@ -8,21 +8,86 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+class SettingsViewController: UIViewController, FBSDKLoginButtonDelegate {
+    var window: UIWindow?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Settings"
         self.view.backgroundColor = UIColor.whiteColor()
 
-        var profileImageView = UIImageView(frame: CGRectMake(25, 80, 150, 150))
+        var profileImageView = UIImageView(frame: CGRectMake(20, 80, 150, 150))
         profileImageView.contentMode = .ScaleAspectFit
         
-//        profileImageView.layer.cornerRadius =  profileImageView.frame.size.height / 2
-//        profileImageView.clipsToBounds = true
+        //profileImageView.layer.cornerRadius =  profileImageView.frame.size.height / 2
+        //profileImageView.clipsToBounds = true
         
         profileImageView.image = GlobalVariables.picture
         self.view.addSubview(profileImageView)
+        
+        // First Name Label
+        var firstNameLabel = UILabel(frame: CGRectMake(185, 120, 180, 40))
+        firstNameLabel.text = GlobalVariables.firstName! + " " + GlobalVariables.lastName!
+        firstNameLabel.font = UIFont(name:"HelveticaNeue", size: 17.0)
+        firstNameLabel.textColor = UIColor.blackColor()
+        self.view.addSubview(firstNameLabel)
+        // End First Name Label
+        
+        // Last Name Label
+        var lastNameLabel = UILabel(frame: CGRectMake(185, 150, 190, 40))
+        lastNameLabel.text = "ravanchi@hotmail.com"
+        lastNameLabel.font = UIFont(name:"HelveticaNeue", size: 17.0)
+        lastNameLabel.textColor = UIColor.blackColor()
+        self.view.addSubview(lastNameLabel)
+        // End Last Name Label
+        
+        // Sign In Button
+        let signInButton = FBSDKLoginButton(frame: CGRectMake(100, 200, 200, 50))
+        signInButton.center = self.view.center
+        self.view.addSubview(signInButton)
+        
+        signInButton.delegate = self
+        // End Sign In Button
+    }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        println("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        let loginViewController = LoginViewController()
+        
+        PFUser.logOut()
+        
+        //let viewController = ViewController()
+        //let navigationController = UINavigationController(rootViewController: loginViewController)
+        //navigationController.navigationBar.backgroundColor = UIColor.blueColor()
+        let currentUser: PFUser? = PFUser.currentUser()
+        
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+
+        self.window?.rootViewController = loginViewController
+
+        presentViewController(navigationController, animated: true, completion: nil)
+        println("User Logged Out")
     }
 }
